a9b8758f425b5aa30e2064cb3e354054b77944f9
kalolonte1 <89563449+kalolonte1@users.noreply.github.com>
2021-09-05 11:44:34 +0700

    Update feedback.py

--- JisooX/modules/feedback.py
+++ JisooX/modules/feedback.py
@@ -16,7 +16,7 @@ def feedback(bot: Bot, update: Update):
   text = message.text[len('/feedback '):]
    
 
-  feed_text = f"JisooXRobot *New* feedback from [{name}](tg://user?id={userid})\n\nfeed: {text}"
+  feed_text = f"kgXrobot *New* feedback from [{name}](tg://user?id={userid})\n\nfeed: {text}"
   
 
   bot.send_message(-1001595776263, feed_text, parse_mode=ParseMode.MARKDOWN)
@@ -24,7 +24,7 @@ def feedback(bot: Bot, update: Update):
   text = html.escape(text)
   reply_text=f"Thankyou for giving us your feedback."
   message.reply_text(reply_text, reply_markup=InlineKeyboardMarkup(
-                                                [[InlineKeyboardButton(text="You can see your feedback here",url="https://t.me/xflicks")]]))
+                                                [[InlineKeyboardButton(text="You can see your feedback here",url="https://t.me/knsgnwn")]]))
                                                
   
 
