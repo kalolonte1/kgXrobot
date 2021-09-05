08e802c3c2412d171f4a0b9de834178411ae0bf7
kalolonte1 <89563449+kalolonte1@users.noreply.github.com>
2021-09-05 11:50:58 +0700

    Update editor.py

--- JisooX/modules/editor.py
+++ JisooX/modules/editor.py
@@ -453,7 +453,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
 __mod_name__ = "ɪᴍᴀɢᴇ ᴇᴅɪᴛᴏʀ"
 __help__ = """
 <b> IMAGE EDITOR </b>
-JisooXRobot have some advanced image editing tools inbuilt
+KGSuperbot have some advanced image editing tools inbuilt
 Bright, Circle, RemBG, Blur, Border, Flip, Glitch, Sticker maker and more
 
 - /edit [reply to image]: Open the image editor
