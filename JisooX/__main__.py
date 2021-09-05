9ab6ef1dc2313145d140602a29c5b6e9a23e110b
kalolonte1 <89563449+kalolonte1@users.noreply.github.com>
2021-09-05 08:54:16 +0700

    Update __main__.py

--- JisooX/__main__.py
+++ JisooX/__main__.py
@@ -178,8 +178,8 @@ def send_start(bot, update):
     first_name = update.effective_user.first_name 
     text = PM_START_TEXT
 
-    keyboard = [[InlineKeyboardButton(text="ʜᴇʟᴘ",callback_data="help_back"),InlineKeyboardButton(text="🛡Creator🛡",url="https://t.me/xflicks")]]
-    keyboard += [[InlineKeyboardButton(text="ᴄᴏɴɴᴇᴄᴛ ɢʀᴏᴜᴘ", callback_data="main_connect"),InlineKeyboardButton(text="⚜️Add Me⚜️",url="t.me/{}?startgroup=true".format(bot.username))]]
+    keyboard = [[InlineKeyboardButton(text="ʜᴇʟᴘ",callback_data="help_back"),InlineKeyboardButton(text="ᴅᴇᴠᴇʟᴏᴘᴇʀ",url="https://t.me/knsgnwn")]]
+    keyboard += [[InlineKeyboardButton(text="ᴄᴏɴɴᴇᴄᴛ ɢʀᴏᴜᴘ", callback_data="main_connect"),InlineKeyboardButton(text="⚜ᴀᴅᴅ ᴍᴇ",url="t.me/{}?startgroup=true".format(bot.username))]]
 
     update.effective_message.reply_photo(img, PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_NAME, OWNER_ID), 
                                          reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
